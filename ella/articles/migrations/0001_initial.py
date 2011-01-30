6a67d4ca24c7c9e6ddd359a37f193099d730265f
Almad <bugs@almad.net>
2011-01-30 16:46:34 +0100

    Upstream migrations unusable for some apps. Create our own.

--- ella/articles/migrations/0001_initial.py
+++ ella/articles/migrations/0001_initial.py
@@ -1,125 +1,175 @@
-
-from south.db import db
-from django.db import models
-from ella.articles.models import *
+# encoding: utf-8
 import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
 
-class Migration:
-
-    depends_on = (
-        ("core", "0001_initial"),
-        ("photos", "0001_initial"),
-    )
+class Migration(SchemaMigration):
 
     def forwards(self, orm):
-
-        # Adding model 'ArticleContents'
-        db.create_table('articles_articlecontents', (
-            ('id', models.AutoField(primary_key=True)),
-            ('article', models.ForeignKey(orm.Article, verbose_name=_('Article'))),
-            ('title', models.CharField(_('Title'), max_length=200, blank=True)),
-            ('content', models.TextField(_('Content'))),
-        ))
-        db.send_create_signal('articles', ['ArticleContents'])
-
+        
         # Adding model 'InfoBox'
         db.create_table('articles_infobox', (
-            ('id', models.AutoField(primary_key=True)),
-            ('title', models.CharField(_('Title'), max_length=255)),
-            ('created', models.DateTimeField(_('Created'), default=datetime.datetime.now, editable=False)),
-            ('updated', models.DateTimeField(_('Updated'), null=True, blank=True)),
-            ('content', models.TextField(_('Content'))),
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('created', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
+            ('updated', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True)),
+            ('content', self.gf('django.db.models.fields.TextField')()),
         ))
         db.send_create_signal('articles', ['InfoBox'])
 
         # Adding model 'Article'
         db.create_table('articles_article', (
-            ('id', models.AutoField(primary_key=True)),
-            ('title', models.CharField(_('Title'), max_length=255)),
-            ('upper_title', models.CharField(_('Upper title'), max_length=255, blank=True)),
-            ('slug', models.SlugField(_('Slug'), max_length=255)),
-            ('perex', models.TextField(_('Perex'))),
-            ('created', models.DateTimeField(_('Created'), default=datetime.datetime.now, editable=False, db_index=True)),
-            ('updated', models.DateTimeField(_('Updated'), null=True, blank=True)),
-            ('source', models.ForeignKey(orm['core.Source'], null=True, verbose_name=_('Source'), blank=True)),
-            ('category', models.ForeignKey(orm['core.Category'], verbose_name=_('Category'))),
-            ('photo', models.ForeignKey(orm['photos.Photo'], null=True, verbose_name=_('Photo'), blank=True)),
+            ('publishable_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['core.Publishable'], unique=True, primary_key=True)),
+            ('upper_title', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
+            ('created', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, db_index=True)),
+            ('updated', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True)),
         ))
         db.send_create_signal('articles', ['Article'])
 
-        # Adding ManyToManyField 'Article.authors'
-        db.create_table('articles_article_authors', (
-            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
-            ('article', models.ForeignKey(orm.Article, null=False)),
-            ('author', models.ForeignKey(orm['core.Author'], null=False))
+        # Adding model 'ArticleContents'
+        db.create_table('articles_articlecontents', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('article', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['articles.Article'])),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=200, blank=True)),
+            ('content', self.gf('django.db.models.fields.TextField')()),
         ))
-
+        db.send_create_signal('articles', ['ArticleContents'])
 
 
     def backwards(self, orm):
-
-        # Deleting model 'ArticleContents'
-        db.delete_table('articles_articlecontents')
-
+        
         # Deleting model 'InfoBox'
         db.delete_table('articles_infobox')
 
         # Deleting model 'Article'
         db.delete_table('articles_article')
 
-        # Dropping ManyToManyField 'Article.authors'
-        db.delete_table('articles_article_authors')
-
+        # Deleting model 'ArticleContents'
+        db.delete_table('articles_articlecontents')
 
 
     models = {
-        'core.category': {
-            'Meta': {'ordering': "('site','tree_path',)", 'unique_together': "(('site','tree_path'),)"},
-            '_stub': True,
-            'id': ('models.AutoField', [], {'primary_key': 'True'})
+        'articles.article': {
+            'Meta': {'object_name': 'Article', '_ormbases': ['core.Publishable']},
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'db_index': 'True'}),
+            'publishable_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['core.Publishable']", 'unique': 'True', 'primary_key': 'True'}),
+            'updated': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'upper_title': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'})
         },
         'articles.articlecontents': {
-            'article': ('models.ForeignKey', ["orm['articles.Article']"], {'verbose_name': "_('Article')"}),
-            'content': ('models.TextField', ["_('Content')"], {}),
-            'id': ('models.AutoField', [], {'primary_key': 'True'}),
-            'title': ('models.CharField', ["_('Title')"], {'max_length': '200', 'blank': 'True'})
+            'Meta': {'object_name': 'ArticleContents'},
+            'article': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['articles.Article']"}),
+            'content': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'})
         },
         'articles.infobox': {
-            'Meta': {'ordering': "('-created',)"},
-            'content': ('models.TextField', ["_('Content')"], {}),
-            'created': ('models.DateTimeField', ["_('Created')"], {'default': 'datetime.datetime.now', 'editable': 'False'}),
-            'id': ('models.AutoField', [], {'primary_key': 'True'}),
-            'title': ('models.CharField', ["_('Title')"], {'max_length': '255'}),
-            'updated': ('models.DateTimeField', ["_('Updated')"], {'null': 'True', 'blank': 'True'})
+            'Meta': {'ordering': "('-created',)", 'object_name': 'InfoBox'},
+            'content': ('django.db.models.fields.TextField', [], {}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'updated': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'})
         },
-        'photos.photo': {
-            'Meta': {'ordering': "('-created',)"},
-            '_stub': True,
-            'id': ('models.AutoField', [], {'primary_key': 'True'})
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
         },
-        'articles.article': {
-            'Meta': {'ordering': "('-created',)"},
-            'authors': ('models.ManyToManyField', ["orm['core.Author']"], {'verbose_name': "_('Authors')"}),
-            'category': ('models.ForeignKey', ["orm['core.Category']"], {'verbose_name': "_('Category')"}),
-            'created': ('models.DateTimeField', ["_('Created')"], {'default': 'datetime.datetime.now', 'editable': 'False', 'db_index': 'True'}),
-            'id': ('models.AutoField', [], {'primary_key': 'True'}),
-            'perex': ('models.TextField', ["_('Perex')"], {}),
-            'photo': ('models.ForeignKey', ["orm['photos.Photo']"], {'null': 'True', 'verbose_name': "_('Photo')", 'blank': 'True'}),
-            'slug': ('models.SlugField', ["_('Slug')"], {'max_length': '255'}),
-            'source': ('models.ForeignKey', ["orm['core.Source']"], {'null': 'True', 'verbose_name': "_('Source')", 'blank': 'True'}),
-            'title': ('models.CharField', ["_('Title')"], {'max_length': '255'}),
-            'updated': ('models.DateTimeField', ["_('Updated')"], {'null': 'True', 'blank': 'True'}),
-            'upper_title': ('models.CharField', ["_('Upper title')"], {'max_length': '255', 'blank': 'True'})
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
         },
-        'core.source': {
-            'Meta': {'ordering': "('name',)"},
-            '_stub': True,
-            'id': ('models.AutoField', [], {'primary_key': 'True'})
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
         },
         'core.author': {
-            'Meta': {'ordering': "('name','slug',)"},
-            '_stub': True,
-            'id': ('models.AutoField', [], {'primary_key': 'True'})
+            'Meta': {'object_name': 'Author'},
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '255', 'db_index': 'True'}),
+            'text': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'null': 'True', 'blank': 'True'})
+        },
+        'core.category': {
+            'Meta': {'ordering': "('site__name', 'tree_path')", 'unique_together': "(('site', 'tree_path'),)", 'object_name': 'Category'},
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'site': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['sites.Site']"}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '255', 'db_index': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'tree_parent': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Category']", 'null': 'True', 'blank': 'True'}),
+            'tree_path': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'core.publishable': {
+            'Meta': {'object_name': 'Publishable'},
+            'authors': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['core.Author']", 'symmetrical': 'False'}),
+            'category': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Category']"}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'photo': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['photos.Photo']", 'null': 'True', 'blank': 'True'}),
+            'publish_from': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(3000, 1, 1, 0, 0, 0, 2)', 'db_index': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '255', 'db_index': 'True'}),
+            'source': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Source']", 'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'core.source': {
+            'Meta': {'object_name': 'Source'},
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'})
+        },
+        'photos.photo': {
+            'Meta': {'ordering': "('-created',)", 'object_name': 'Photo'},
+            'authors': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'photo_set'", 'symmetrical': 'False', 'to': "orm['core.Author']"}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'height': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'important_bottom': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'important_left': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'important_right': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'important_top': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '255', 'db_index': 'True'}),
+            'source': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Source']", 'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'width': ('django.db.models.fields.PositiveIntegerField', [], {})
+        },
+        'sites.site': {
+            'Meta': {'ordering': "('domain',)", 'object_name': 'Site', 'db_table': "'django_site'"},
+            'domain': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
         }
     }
 
